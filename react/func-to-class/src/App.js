a4c69350272831db218fcfef0c74ef8d91e84294
KIM JAE HYUN <geoblo@naver.com>
2022-04-20 18:49:06 +0900

    4.20 react study start

---
+++ react/func-to-class/src/App.js
@@ -0,0 +1,13 @@
+import StartClass from './components/StartClass';
+import StartFunc from './components/StartFunc';
+
+function App() {
+  return (
+    <>
+      <StartClass />
+      <StartFunc />
+    </>
+  );
+}
+
+export default App;
---
+++ react/func-to-class/src/components/StartFunc.js
@@ -0,0 +1,13 @@
+// import React from "react";
+
+// 클래스로 만드는 컴포넌트
+function StartFunc() {
+  return (
+    <div>
+      <h1>function 컴포넌트</h1>
+      <p>html 적용하기</p>
+    </div>
+  );
+}
+
+export default StartFunc;
