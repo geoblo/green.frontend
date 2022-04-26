31cc12c0ac5ed4411deb42920e1020e3ad8a2584
KIM JAE HYUN <geoblo@naver.com>
2022-04-26 18:57:25 +0900

    4.26 react study

---
+++ react/css-test/src/App.js
@@ -0,0 +1,13 @@
+import CSSModule from './CSSModule';
+import SassComponent from './SassComponent';
+import StyledComponent from './StyledComponent';
+
+function App() {
+  return (
+    // <SassComponent />
+    // <CSSModule />
+    <StyledComponent />
+  );
+}
+
+export default App;
