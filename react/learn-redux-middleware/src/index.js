abbd94d2f5854f720d523feac1fa073d89609488
KIM JAE HYUN <geoblo@naver.com>
2022-05-11 18:58:51 +0900

    5.11 react

--- react/learn-redux-middleware/src/index.js
+++ react/learn-redux-middleware/src/index.js
@@ -2,17 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from "./modules";
-// import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer, { rootSaga } from "./modules";
+import { composeWithDevTools } from 'redux-devtools-extension';
 // import loggerMiddleware from './lib/loggerMiddleware';
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
+import createSagaMiddleware from "redux-saga";
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(ReduxThunk, sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -24,4 +31,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
--- react/learn-redux-middleware/src/modules/index.js
+++ react/learn-redux-middleware/src/modules/index.js
@@ -1,7 +1,8 @@
 // 루트 리듀서: 기존에 만들었던 리듀서들을 하나로 합쳐준다.
 import { combineReducers } from "redux";
-import counter from "./counter";
-import sample from "./sample";
+import { all } from "redux-saga/effects";
+import counter, { counterSaga } from "./counter";
+import sample, { sampleSaga } from "./sample";
 import loading from "./loading";
 
 const rootReducer = combineReducers({
@@ -9,5 +10,9 @@ const rootReducer = combineReducers({
   sample,
   loading
 });
+
+export function* rootSaga() {
+  yield all([counterSaga(), sampleSaga()]);
+}
 
 export default rootReducer;
